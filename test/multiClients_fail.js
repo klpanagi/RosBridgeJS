8cc6d2f72739b1f5e0fb51260addc0ad333116f5
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-10-05 00:55:42 +0300

    Multi Clients test

---
+++ test/multiClients_fail.js
@@ -0,0 +1,20 @@
+
+var ROS = require( __dirname + '/../src/Rosbridge.js');
+var ros = new ROS('','');
+var timeout = 1000;
+var srvName = [];
+var CLIENTS = 50;
+for(i=0;i<CLIENTS;i++)
+{
+  srvName.push('/srv' + i.toString());
+}
+
+var args = {};
+
+setTimeout( function(){
+  for(i in srvName){
+    ros.callService(srvName[i], args, function(data){
+      console.log(data);
+    });
+  }
+}, timeout);
