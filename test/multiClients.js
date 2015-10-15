178e7b47b804b02c12ee9c3483b94b279d58f0f2
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-10-15 21:29:20 +0300

    Tests for closeConnection requests and openClose stressed

--- test/multiClients.js
+++ test/multiClients.js
@@ -4,15 +4,17 @@ var ros = new ROS('','');
 var timeout = 1000;
 var srvName = [];
 var CLIENTS = 50;
+
 for(i=0;i<CLIENTS;i++)
 {
-  srvName.push('/srv' + i.toString());
+  srvName.push('/rosapi/topics');
 }
 
 var args = {};
 
 setTimeout( function(){
   for(i in srvName){
+    console.log('[Client %s]', i)
     ros.callService(srvName[i], args, function(data){
       console.log(data);
     });
